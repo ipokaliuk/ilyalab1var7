33eab40a5f7cabacc2e005b286b6e83a805beb46
Паша Ярошенко <pavlo.legominecraft@gmail.com>
2024-04-26 11:45:10 +0300

    Add project files.

---
+++ ilyalab1/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ilyalab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxRed = new System.Windows.Forms.TextBox();
+            this.textBoxBlue = new System.Windows.Forms.TextBox();
+            this.textBoxGreen = new System.Windows.Forms.TextBox();
+            this.textBoxColor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxRed
+            // 
+            this.textBoxRed.Location = new System.Drawing.Point(279, 93);
+            this.textBoxRed.Name = "textBoxRed";
+            this.textBoxRed.Size = new System.Drawing.Size(100, 26);
+            this.textBoxRed.TabIndex = 0;
+            this.textBoxRed.TextChanged += new System.EventHandler(this.textBoxRed_TextChanged);
+            // 
+            // textBoxBlue
+            // 
+            this.textBoxBlue.Location = new System.Drawing.Point(279, 196);
+            this.textBoxBlue.Name = "textBoxBlue";
+            this.textBoxBlue.Size = new System.Drawing.Size(100, 26);
+            this.textBoxBlue.TabIndex = 1;
+            this.textBoxBlue.TextChanged += new System.EventHandler(this.textBoxBlue_TextChanged_1);
+            // 
+            // textBoxGreen
+            // 
+            this.textBoxGreen.Location = new System.Drawing.Point(279, 145);
+            this.textBoxGreen.Name = "textBoxGreen";
+            this.textBoxGreen.Size = new System.Drawing.Size(100, 26);
+            this.textBoxGreen.TabIndex = 2;
+            this.textBoxGreen.TextChanged += new System.EventHandler(this.textBoxGreen_TextChanged_1);
+            // 
+            // textBoxColor
+            // 
+            this.textBoxColor.Location = new System.Drawing.Point(279, 278);
+            this.textBoxColor.Name = "textBoxColor";
+            this.textBoxColor.Size = new System.Drawing.Size(100, 26);
+            this.textBoxColor.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxColor);
+            this.Controls.Add(this.textBoxGreen);
+            this.Controls.Add(this.textBoxBlue);
+            this.Controls.Add(this.textBoxRed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxRed;
+        private System.Windows.Forms.TextBox textBoxBlue;
+        private System.Windows.Forms.TextBox textBoxGreen;
+        private System.Windows.Forms.TextBox textBoxColor;
+    }
+}
+
---
+++ ilyalab1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ilyalab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxBlue.Text = "0";
+            textBoxGreen.Text = "0";
+            textBoxRed.Text = "0";
+        }
+
+        private void UpdateColor()
+        {
+            int red, green, blue;
+            if (int.TryParse(textBoxRed.Text, out red) && int.TryParse(textBoxGreen.Text, out green) && int.TryParse(textBoxBlue.Text, out blue))
+            {
+                if (red >= 0 && red <= 255 && green >= 0 && green <= 255 && blue >= 0 && blue <= 255)
+                {
+                    Color color = Color.FromArgb(red, green, blue);
+                    textBoxColor.BackColor = color;
+                }
+                else
+                {
+                    MessageBox.Show("Кожне значення кольору повинно бути між 0 та 255", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Будь ласка, введіть коректні числа для кольорів", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBoxRed_TextChanged(object sender, EventArgs e)
+        {
+            UpdateColor();
+        }
+
+        private void textBoxGreen_TextChanged_1(object sender, EventArgs e)
+        {
+           UpdateColor();
+        }
+
+        private void textBoxBlue_TextChanged_1(object sender, EventArgs e)
+        {
+            UpdateColor();
+        }
+    }
+}
